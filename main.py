51599d7364e00d153c07b10270f1c7e4752678e6
bradleyowenthompson <bradleyowenthompson@outlook.com>
2022-11-19 07:06:50 +0000

    Refactored, implemented as class

--- main.py
+++ main.py
@@ -10,45 +10,67 @@
     make the dataset "human-readable" for analysis. Files can be joined using the 'AccidentIndex'
     column present in the datasets.
 """
-
 import json
 import os
 import wget
 
-def init():
-    """ Configures The Director And Downloads Missing Files """
+class DatasetProcessor:
+    """
+    This script downloads the accident, vehicle, and casualties files, and uses the lookup table to
+    make the dataset "human-readable" for analysis. Files can be joined using the 'AccidentIndex'
+    column present in the datasets.
+    """
 
-    # Read config.json
-    with open('config.json', 'r', encoding='UTF-8') as file:
-        print("Reading config.json")
+    def __init__(self) -> None:
+        """ Constructor, defining variables """
 
-        json_config = json.load(file)
-        temp_dir = json_config["Config"]["Download Dir"]
-        data_files = json_config["Files"]
+        self.__temp_dir = None        # type: str
+        self.__data_files = {}        # type: dict
 
-    # Check If The Download Folder Exists
-    if not os.path.exists(temp_dir):
-        # Create Folder If Doesn't Existit does not exist
-        os.makedirs(temp_dir)
-        print("New Directory 'temp' Created")
+        # Process config.json
+        self.__process_config_file()
+        self.__check_data_exists()
 
-    # Iterate Over Files Required For Processing
-    for file in data_files:
-        filename = data_files[file]["friendly file name"]
-        download_url = data_files[file]["url"]
+    
+    def __process_config_file(self) -> None:
+        """ Configures The Director And Downloads Missing Files """
 
-        # Check If Files Are Downloaded
-        if not os.path.isfile(f"{temp_dir}/{filename}"):
-            print(f"Downloading {filename}")
+        # Read config.json
+        with open('config.json', 'r', encoding='UTF-8') as file:
+            print("Reading config.json")
 
-            # Download Missing Files
-            downloaded_file = wget.download(download_url, out=f"{temp_dir}/")
-            os.rename(downloaded_file, f"{temp_dir}/{filename}")
+            json_config = json.load(file)
+            self.__temp_dir = json_config["Config"]["Download Dir"]
+            self.__data_files = json_config["Files"]
 
-            print(f"\nDownload Complete - {filename}")
-        else:
-            print(f"File Already Exists - {filename}")
+    
+    def __check_data_exists(self) -> None:
+        """ Check if the temp folder and data exist, download missing data """
+
+        # Check If The Download Folder Exists
+        if not os.path.exists(self.__temp_dir):
+            # Create Folder If Doesn't Existit does not exist
+            os.makedirs(self.__temp_dir)
+            print("New Directory 'temp' Created")
+
+        # Iterate Over Files Required For Processing
+        for file in self.__data_files:
+            filename = self.__data_files[file]["friendly file name"]
+            download_url = self.__data_files[file]["url"]
+
+            # Check If Files Are Downloaded
+            if not os.path.isfile(f"{self.__temp_dir}/{filename}"):
+                print(f"Downloading {filename}\n")
+
+                # Download Missing Files
+                downloaded_file = wget.download(download_url, out=f"{self.__temp_dir}/")
+                os.rename(downloaded_file, f"{self.__temp_dir}/{filename}")
+
+                print(f"\n Download Complete - {filename} \n")
+            else:
+                print(f"File Already Exists - {filename}")
 
 
 if __name__ == '__main__':
-    init()
+    # Create new Data Processor
+    processor = DatasetProcessor()
